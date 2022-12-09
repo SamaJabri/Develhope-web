c16865c5a07e7e5c7715956d57a6aacf316bf36d
Sama Jabri <sama.jabri@outlook.com>
2022-12-09 10:30:52 +0300

    Solved exercises upto 10

--- Chapter 7 - TypeScript/Exercise 01/exercise-1-primitive-types-start.ts
+++ Chapter 7 - TypeScript/Exercise 01/exercise-1-primitive-types-start.ts
@@ -4,23 +4,23 @@
 
 // Add type annotations to each of these variables.
 
-let country: string = "Italy";
+let country : string = "Italy";
 
-let year: undefined = undefined;
+let year : undefined = undefined;
 
-let averageTemperature: number = 23.6;
+let averageTemperature : number = 23.6;
 
-let visited: boolean = true;
+let visited : boolean = true;
 
-let currency: null = null;
+let currency : null = null;
 
 // Change the type annotations on these variables so they are correct.
 
-let population: number = 47_450_795;
+let population : number = 47_450_795;
 
-let isSummer: boolean = false;
+let isSummer : boolean = false;
 
-let languages: string = "Spanish, English";
+let languages : string = "Spanish, English";
 
 // ----
 
